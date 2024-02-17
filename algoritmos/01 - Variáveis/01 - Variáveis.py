6e437af6691b9f33b85a0a460b8c5033d4c09432
Victória Mendes <89945193+vmc13@users.noreply.github.com>
2024-02-17 17:00:31 -0300

    Add files via upload

---
+++ algoritmos/01 - Variáveis/01 - Variáveis.py
@@ -0,0 +1,58 @@
+# Questão 01
+nome = "Victória Mendes"
+# print(f"Meu nome é {nome}.")
+
+# Questão 02
+nome2 = "Victória Mendes"
+curso = "Licenciatura em Computação"
+# print(f'Meu nome é {nome2}')
+# print(f'Meu curso é {curso}')
+
+# Questão 03
+nome3 = "Victória Mendes"
+idade = 20
+print(f'Meu nome é {nome3} e minha idade é {idade}.')
+
+# Questão 04
+a = 3
+b = 5
+resultado = (2*a)*(3*b)
+print(f'O resultado é {resultado}') # 90
+
+# Questão 05
+num1 = 10
+num2 = 5 
+num3 = 8
+soma = num1+num2+num3
+print(f'A soma de {num1}, {num2} e {num3} é igual a {soma}.') # 23
+
+# Questão 06
+salario = 1050
+imposto = salario*0.2
+print(f'O imposto a ser pago é R${imposto:.2f}.') # 210
+
+# Questão 07
+base = 8
+altura = 6
+area = (base*altura)/2
+print(f'A área do triângulo é {area:.0f}.') # 24
+
+# Questão 08
+nota1 = 10
+nota2 = 5.8
+nota3 = 6.7
+media = (nota1+nota2+nota3)/3
+print(f'A sua média é: {media:.1f}.') # 7.5
+
+# Questão 09
+salario_bruto = 2500
+imposto2 = salario_bruto*0.15
+salario_liquido = salario_bruto - imposto2
+print(f'O seu salário líquido é de R${salario_liquido:.2f}.') # 2125.00
+
+# Questão 10
+salario_bruto10 = 5000
+inss = 5000*0.15
+ir = 5000*0.1
+salario_liquido10 = salario_bruto10 - inss - ir
+print(f'O valor descontado pelo INSS é de R${inss:.2f}, o valor descontado pelo IR é de R${ir:.2f} e o salário líquido é de R${salario_liquido10}.') # INSS = 750, IR = 500, Líquido = 3750
